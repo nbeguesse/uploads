ff200f07c039f6942d52320a00a202c47e27844c
Nicole Beguesse <nicole@highlinesale.com>
2014-02-20 00:51:49 -0500

    Basic login functionality works

--- www/js/main.js
+++ www/js/main.js
@@ -1,14 +1,106 @@
-window.HomeView = Backbone.View.extend({
+window.hls = {};
+hls.user = null;
 
+hls.HlsModel = Backbone.Model.extend({
+    getUrl:function(url, options){
+      var model = this;
+      var options = options;
+      options || (options = {});
+      options.data || (options.data = {});
+      options.dataType || (options.dataType = "GET");
+      var success = options.success;
+      var error = options.error || this.defaultError;
+      // if (ss.user && ss.user.mobile){
+      //   options.data = addAccessToken(options.data)
+      // }
+      this.set({isLoadingUrl:true});
+      $.mobile.loading('show');
+      $.ajax({
+        type: options.dataType,
+        url: url,
+        data:options.data,
+        success:function(data){
+          model.set({isLoadingUrl:false});
+          $.mobile.loading('hide');
+            if(success){ 
+              success(data); 
+            }
+        },
+        dataType: "jsonp"
+      });
+    },
+    defaultError:function(){
+      alert('There was an error contacting the server. Please try again later.');
+    },
+});
+hls.Car = Backbone.Model.extend({
+    initialize:function(){
+        console.log('Creating Car:', this.attributes);
+    },
+    description:function(){
+        var parts = [this.attributes.year, this.attributes.make, this.attributes.model];
+        return parts.join(" ");
+    }
+});
+hls.UserModel = Backbone.Model.extend({
+    initialize:function(){
+        console.log('Creating User:', this.attributes);
+        this.cars = new hls.CarList( this.attributes.own_cars);
+        this.cars.user = this;
+        //TODO: Save User in local storage, then use to initialize hls.user
+    }
+});
+hls.UserSession = hls.HlsModel.extend({
+  url:"http://localhost:3000/user_sessions/create.json",
+  login:function(){
+    var data = $("#login-form").serialize();
+    this.getUrl(this.url,{
+        data:data,
+        success:function(data){
+          if(data.errors){
+            alert(data.errors[0]); //tell them why they can't login
+          } else {
+            hls.user = new hls.UserModel(data.user);
+            app.navigate("login",true);
+          }
+        }
+    });
+  },
+});
+
+
+
+hls.CarList = Backbone.Collection.extend({
+  model:hls.Car
+});
+
+
+hls.HomeView = Backbone.View.extend({
+    events: {
+      'click button.login': '_login'
+    },
     template:_.template($('#home').html()),
 
     render:function (eventName) {
         $(this.el).html(this.template());
         return this;
+    },
+    _login:function(e){
+        e.preventDefault();
+        var session = new hls.UserSession();
+        session.login();
+        return;
     }
 });
+hls.UserView = Backbone.View.extend({
 
-window.Page1View = Backbone.View.extend({
+    render:function (eventName) {
+        var template = _.template($('#user').html(),{user:this.model});
+        $(this.el).html(template);
+        return this;
+    }
+});
+hls.Page1View = Backbone.View.extend({
 
     template:_.template($('#page1').html()),
 
@@ -18,7 +110,7 @@ window.Page1View = Backbone.View.extend({
     }
 });
 
-window.Page2View = Backbone.View.extend({
+hls.Page2View = Backbone.View.extend({
 
     template:_.template($('#page2').html()),
 
@@ -28,12 +120,14 @@ window.Page2View = Backbone.View.extend({
     }
 });
 
-var AppRouter = Backbone.Router.extend({
+hls.AppRouter = Backbone.Router.extend({
 
     routes:{
         "":"home",
+        "login":"login",
         "page1":"page1",
-        "page2":"page2"
+        "page2":"page2",
+
     },
 
     initialize:function () {
@@ -47,17 +141,28 @@ var AppRouter = Backbone.Router.extend({
 
     home:function () {
         console.log('#home');
-        this.changePage(new HomeView());
+        if(hls.user){
+            this.changePage(new hls.UserView({model:hls.user}));
+        } else {
+            this.changePage(new hls.HomeView());
+        }
     },
-
+    login:function () {
+        console.log('#login');
+        if(hls.user){
+            this.changePage(new hls.UserView({model:hls.user}));
+        } else {
+            this.changePage(new hls.HomeView());
+        }
+    },
     page1:function () {
         console.log('#page1');
-        this.changePage(new Page1View());
+        this.changePage(new hls.Page1View());
     },
 
     page2:function () {
         console.log('#page2');
-        this.changePage(new Page2View());
+        this.changePage(new hls.Page2View());
     },
 
     changePage:function (page) {
@@ -77,6 +182,6 @@ var AppRouter = Backbone.Router.extend({
 
 $(document).ready(function () {
     console.log('document ready');
-    app = new AppRouter();
+    app = new hls.AppRouter();
     Backbone.history.start();
 });
